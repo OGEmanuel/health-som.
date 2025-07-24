83aa6bd1eebf06ac82a33e6e5adb841ac8c71f22
OGEmanuel <1blacksanta416@gmail.com>
2025-07-24 23:47:23 +0100

    add:account dropdown

--- src/assets/jsx-icons/lock.tsx
+++ src/assets/jsx-icons/lock.tsx
@@ -1,6 +1,8 @@
-const Lock = () => {
+const Lock = (props: { stroke?: string; className?: string }) => {
+  const { stroke = '#ACACAC', className } = props;
   return (
     <svg
+      className={className}
       width="20"
       height="20"
       viewBox="0 0 20 20"
@@ -9,7 +11,7 @@ const Lock = () => {
     >
       <path
         d="M13.3333 9.99992H14.4C14.7314 9.99992 15 10.2685 15 10.5999V16.0666C15 16.398 14.7314 16.6666 14.4 16.6666H5.6C5.26863 16.6666 5 16.398 5 16.0666V10.5999C5 10.2685 5.26863 9.99992 5.6 9.99992H6.66667M13.3333 9.99992V6.66659C13.3333 5.55547 12.6667 3.33325 10 3.33325C7.33333 3.33325 6.66667 5.55547 6.66667 6.66659V9.99992M13.3333 9.99992H6.66667"
-        stroke="#ACACAC"
+        stroke={stroke}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/home/medical-reports.tsx
+++ src/home/medical-reports.tsx
@@ -78,7 +78,7 @@ const REPORTS: Report[] = [
 const MedicalReports = () => {
   return (
     <section className="flex w-full max-w-[1032px] flex-col gap-8 self-start">
-      <ChangePassword />
+      {/* <ChangePassword /> */}
       <div className="flex flex-col gap-2 sm:gap-3">
         <h1 className="text-xl leading-[100%] font-semibold -tracking-[0.03em] sm:text-2xl">
           Welcome, Abolaji
@@ -90,7 +90,7 @@ const MedicalReports = () => {
       </div>
       <div className="custom-dashed-border"></div>
       {REPORTS.length > 0 ? (
-        <div className="grid grid-cols-2 gap-x-5 gap-y-[26px] pb-[4.3125rem] sm:gap-x-6 sm:gap-y-8 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-x-5 gap-y-[26px] max-sm:pb-[4.3125rem] sm:gap-x-6 sm:gap-y-8 md:grid-cols-3 lg:grid-cols-4">
           {REPORTS.map(report => (
             <ReportList key={report.id} report={report} />
           ))}
